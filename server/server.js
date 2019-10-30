a5098e9a3dd5f64f5c1275da23d4a47ea2997b0c
Anton Markaj <anton.markaj@students.ffhs.ch>
2019-10-30 07:09:26 +0100

    Push to remote origin

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile('C:/Users/marka/WebstormProjects/Montagsmaler/client/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
